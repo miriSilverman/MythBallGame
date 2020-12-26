0ef89f123eb623fcae83ecf17129a147206d9b0d
kfir <kfir.hayman@gmail.com>
2020-12-27 01:35:20 +0200

    level 4 colliders done

---
+++ Assets/Scripts/Portals.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portals : MonoBehaviour
+{
+    [SerializeField] private Collider portal1;
+    [SerializeField] private Collider portal2;
+    private void OnCollisionEnter(Collision other)
+    {
+        foreach (ContactPoint contact in other.contacts)
+        {
+            
+            
+            if (contact.thisCollider.Equals(portal1) && contact.otherCollider.CompareTag("Ball"))
+            {
+                Vector3 pos = portal2.transform.up;
+                GameObject Ball = Instantiate(Resources.Load("Ball")) as GameObject;
+                
+                
+                Ball.transform.position = portal2.transform.position;
+                Rigidbody oldRb = contact.otherCollider.GetComponent<Rigidbody>();
+                Rigidbody rb = Ball.GetComponent<Rigidbody>();
+                rb = oldRb;
+                rb.velocity = pos * oldRb.velocity.magnitude;
+                GameObject oldBall = contact.otherCollider.gameObject;
+                Destroy(oldBall);
+            }
+            
+            
+
+        }
+
+    }
+    
+    
+    
+}
